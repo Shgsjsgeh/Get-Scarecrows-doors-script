b00a7759078968973c2405cb1d7323fab2715be8
Shgsjsgeh <ssteelwoolgamesjulian@gmail.com>
2024-10-12 09:08:41 -0400

    Create DoorEsp.lua

---
+++ DoorEsp.lua
@@ -0,0 +1,42 @@
+while true do
+local door = workspace.CurrentRooms:FindFirstChild("0").Door
+
+if door then
+    local highlight = Instance.new("Highlight")
+    highlight.FillColor = Color3.fromRGB(255, 0, 0)
+    highlight.OutlineColor = Color3.fromRGB(255, 255, 255)
+    highlight.FillTransparency = 0.5
+    highlight.OutlineTransparency = 0
+    highlight.Parent = door
+end
+local door = workspace.CurrentRooms:FindFirstChild("1").Door
+
+if door then
+    local highlight = Instance.new("Highlight")
+    highlight.FillColor = Color3.fromRGB(255, 0, 0)
+    highlight.OutlineColor = Color3.fromRGB(255, 255, 255)
+    highlight.FillTransparency = 0.5
+    highlight.OutlineTransparency = 0
+    highlight.Parent = door
+end
+local door = workspace.CurrentRooms:FindFirstChild("2").Door
+
+if door then
+    local highlight = Instance.new("Highlight")
+    highlight.FillColor = Color3.fromRGB(255, 0, 0)
+    highlight.OutlineColor = Color3.fromRGB(255, 255, 255)
+    highlight.FillTransparency = 0.5
+    highlight.OutlineTransparency = 0
+    highlight.Parent = door
+end
+local door = workspace.CurrentRooms:FindFirstChild("3").Door
+
+if door then
+    local highlight = Instance.new("Highlight")
+    highlight.FillColor = Color3.fromRGB(255, 0, 0)
+    highlight.OutlineColor = Color3.fromRGB(255, 255, 255)
+    highlight.FillTransparency = 0.5
+    highlight.OutlineTransparency = 0
+    highlight.Parent = door
+end
+wait()
