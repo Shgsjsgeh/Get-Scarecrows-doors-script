aba8bc15f3b993ba63581a33e5a7af7ea633177e
Shgsjsgeh <ssteelwoolgamesjulian@gmail.com>
2024-10-12 20:54:11 -0400

    Update Anti figure hearing.lua

--- Anti figure hearing.lua
+++ Anti figure hearing.lua
@@ -1,5 +1,9 @@
-local args = {
-    [1] = true
-}
+while true do
+    local args = {
+        [1] = true
+    }
 
-game:GetService("ReplicatedStorage").RemotesFolder.Crouch:FireServer(unpack(args))
+    game:GetService("ReplicatedStorage").RemotesFolder.Crouch:FireServer(unpack(args))
+    
+    wait(0.1)
+end
